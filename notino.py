eb345ed4177652f5ad421348a3dffef3e8847a99
Klára Martinásková <martinasek.klara@gmail.com>
2023-08-19 15:59:39 +0200

    load data and save as df

--- notino.py
+++ notino.py
@@ -1,2 +1,25 @@
 # Notino Test Assigment
 # Author: Klara Martinaskova
+# Task: Evaluate an AB test of the recommendation algorithm
+
+#!/usr/bin/env python3.9
+# coding=utf-8
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import zipfile
+import os
+import csv
+
+
+# # open CSV files
+# with open('clients_final.csv', 'r') as file:
+#     csv_clients = csv.reader(file)
+# with open('orders_final.csv', 'r') as file:
+#     csv_orders = csv.reader(file)
+
+# open csv file ans save as dataframe
+df_clients = pd.read_csv('clients_final.csv',encoding="cp1250", sep=",", low_memory=False)
+df_orders = pd.read_csv('orders_final.csv',encoding="cp1250", sep=",", low_memory=False)
